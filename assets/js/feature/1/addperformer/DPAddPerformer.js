158d7477e5e63cdec5221bd57ff1142545d9571c
Justin Wickenheiser <wickenhj@gmail.com>
2020-09-07 10:17:50 -0400

    post an alert to prevent adding performers when not on count 0. this is more for my sanity.

--- assets/js/feature/1/addperformer/DPAddPerformer.js
+++ assets/js/feature/1/addperformer/DPAddPerformer.js
@@ -4,63 +4,72 @@ class DPAddPerformer extends DPFeature {
 	}
 
 	onclick() {
-		dpEditor.view.onMouseUp = function(event) {
-			var position;
-			if (event.event.shiftKey) {
-				// "snap to grid. i.e. round to something divisible by pps"
-				position = new paper.Point( [Math.round(event.point.x/dpEditor.settings.pps)*dpEditor.settings.pps, Math.round(event.point.y/dpEditor.settings.pps)*dpEditor.settings.pps] );
-			} else {
-				position = event.point;
-			}
-			
-			var newPerf = new DPPerformer({
-				'performerId': uuidv4(),
-				'drillNumber': {
-					number: dpEditor.getDPPerformers().length + 1
-				},
-				// Set some of the Paper PointText properties
-				content: 'X',
-				point: position,
-				fillColor: 'black',
-				fontFamily: 'Arial'
-			});
-			// this needs to be reset because adding the Content shifts the size of the bounding box
-			newPerf.position = position;
-			// we manually updated position, so update the DrillNum position too
-			newPerf.updateDrillNumberPosition();
+		var chartIdx = dpEditor.getActiveChartIdx(),
+			chart = dpEditor.getDPChart(chartIdx),
+			countIdx = chart.getActiveCountIdx();
 
-			// Give the performer the ability to be dragged.
-			newPerf.onMouseDrag = function(event) {
-				var chartIdx = dpEditor.getActiveChartIdx();
-				var chart = dpEditor.getDPChart(chartIdx);
-				var countIdx = chart.getActiveCountIdx();
-				var chartId = chart.getChartId();
-
-				// update the position visually
+		if (countIdx != 0) {
+			alert('You can only add new performers when on Count 0 of the chart.');
+			dpEditor.clearActiveFeature();
+		} else {
+			dpEditor.view.onMouseUp = function(event) {
+				var position;
 				if (event.event.shiftKey) {
 					// "snap to grid. i.e. round to something divisible by pps"
-					this.position = [Math.round(event.point.x/dpEditor.settings.pps)*dpEditor.settings.pps, Math.round(event.point.y/dpEditor.settings.pps)*dpEditor.settings.pps];
+					position = new paper.Point( [Math.round(event.point.x/dpEditor.settings.pps)*dpEditor.settings.pps, Math.round(event.point.y/dpEditor.settings.pps)*dpEditor.settings.pps] );
 				} else {
-					this.position = this.position.add(event.delta);
+					position = event.point;
 				}
-				// update the drillNumber position
-				this.updateDrillNumberPosition();
-				// TO-DO:
-				// Update the position for the active chart & count
-				this.getPositionSet(chartId, countIdx).x = this.position.x;
-				this.getPositionSet(chartId, countIdx).y = this.position.y;
+				
+				var newPerf = new DPPerformer({
+					'performerId': uuidv4(),
+					'drillNumber': {
+						number: dpEditor.getDPPerformers().length + 1
+					},
+					// Set some of the Paper PointText properties
+					content: 'X',
+					point: position,
+					fillColor: 'black',
+					fontFamily: 'Arial'
+				});
+				// this needs to be reset because adding the Content shifts the size of the bounding box
+				newPerf.position = position;
+				// we manually updated position, so update the DrillNum position too
+				newPerf.updateDrillNumberPosition();
+
+				// Give the performer the ability to be dragged.
+				newPerf.onMouseDrag = function(event) {
+					var chartIdx = dpEditor.getActiveChartIdx();
+					var chart = dpEditor.getDPChart(chartIdx);
+					var countIdx = chart.getActiveCountIdx();
+					var chartId = chart.getChartId();
+
+					// update the position visually
+					if (event.event.shiftKey) {
+						// "snap to grid. i.e. round to something divisible by pps"
+						this.position = [Math.round(event.point.x/dpEditor.settings.pps)*dpEditor.settings.pps, Math.round(event.point.y/dpEditor.settings.pps)*dpEditor.settings.pps];
+					} else {
+						this.position = this.position.add(event.delta);
+					}
+					// update the drillNumber position
+					this.updateDrillNumberPosition();
+					// TO-DO:
+					// Update the position for the active chart & count
+					this.getPositionSet(chartId, countIdx).x = this.position.x;
+					this.getPositionSet(chartId, countIdx).y = this.position.y;
+				}
+
+				// Set the position in the performer's positionSet
+				var chartIdx = dpEditor.getActiveChartIdx();
+				var chart = dpEditor.getDPChart(chartIdx);
+				// initialize this performer w/ empty [0,0] point for all charts
+				newPerf.initializePositionSets(dpEditor);
+				// set current position for activeCountIdx in the current activeChart
+				newPerf.setPositionSet(position, chart.getChartId(), chart.getActiveCountIdx());
+
+				// Add this new performer to the editor
+				dpEditor.setDPPerformer( newPerf );
 			}
-
-			// Set the position in the performer's positionSet
-			var chartIdx = dpEditor.getActiveChartIdx();
-			var chart = dpEditor.getDPChart(chartIdx);
-			// initialize this performer w/ empty [0,0] point for all charts
-			newPerf.initializePositionSets(dpEditor);
-			// set current position for activeCountIdx in the current activeChart
-			newPerf.setPositionSet(position, chart.getChartId(), chart.getActiveCountIdx());
-
-			// Add this new performer to the editor
-			dpEditor.setDPPerformer( newPerf );
 		}
 	}
 

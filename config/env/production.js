e77624814ac990aae4f29030e09a54ceb89934be
Justin Wickenheiser <wickenhj@gmail.com>
2020-09-30 10:14:31 -0400

    more testing with adapters because it seems to have broken production

--- config/env/production.js
+++ config/env/production.js
@@ -47,7 +47,7 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     default: {
-      adapter: 'sails-postgresql',
+      adapter: require('sails-postgresql'),
       url: process.env.DATABSE_URL,
       // url: sails_datastores__default__url,
       //--------------------------------------------------------------------------

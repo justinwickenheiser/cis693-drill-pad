92831de76667981b319d9860ea64672ad568799b
Justin Wickenheiser <wickenhj@gmail.com>
2020-11-27 12:27:12 -0500

    trying for production this time

--- config/env/production.js
+++ config/env/production.js
@@ -327,6 +327,12 @@ module.exports = {
     ***************************************************************************/
     trustProxy: true,
 
+    bodyParser: (function _configureBodyParser(){
+      var skipper = require('skipper');
+      var middlewareFn = skipper({ strict: true, limit:10000000, parameterLimit:10000 });
+      return middlewareFn;
+    })(),
+
   },
 
 
